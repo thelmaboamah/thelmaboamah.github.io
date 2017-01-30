d44be2f4dbadf78ca39150fc4e1e976c88de2d72
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-01-29 17:23:00 -0800

    home page done, started portfolio page

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,23 @@
 console.log("app.js linked.");
 
 $(document).ready(function() {
-
-	$("nav img").click(function(){
+	$("header img").click(function(){
 		$(this).removeClass("infinite");
+		$(".main-nav").slideToggle(500);
+		
 	});
 
+	$(".fa-chevron-circle-up").click(function(){
+		$(this).parent(".box-front").hide();
+		var boxBack = $(this).parent(".box-front").siblings(".box-back");
+		boxBack.show();
+	});
 
-
+	$(".fa-times").click(function(){
+		$(this).parent(".box-back").hide();
+		var boxFront = $(this).parent(".box-back").siblings(".box-front")
+		boxFront.show();
+	});
 
 
 });
