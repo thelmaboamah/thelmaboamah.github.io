779a7457e3396b14b2f3c4bf833aa38ea1e1ff5d
Thelma Boamah <boamah@adobe.com>
2017-05-21 01:03:26 -0700

    add more projects and update about me

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,19 +20,37 @@ $(document).ready(function() {
 		boxFront.show();
 	});
 
+	var baseUrl="http://thelmaboamah.github.io"
+
 	var projects = [
 		{
-		image:"assets/imgs/hillarymyths.png",
-		alt: "Screenshot of Hillary Myths dot org",
-		url: "https://hillarymyths.org/",	
-		name: "Hillary Myths",
-		description: "A question and answer site with the goal of debunking common misconceptions about Hillary Clinton during the 2016 election.",
-		repoUrl:"https://github.com/DevProgress/i-like-hillary-but"
+		image:"assets/imgs/closeup.png",
+		alt: "Screenshot of Close Up app",
+		url: "https://close-up.herokuapp.com/",	
+		name: "Close Up",
+		description: "Photo capture and CSS filters",
+		repoUrl:"https://github.com/thelmaboamah/ga-final-project"
+		},
+		{
+		image:"assets/imgs/podme.png",
+		alt: "Screenshot of Podme app",
+		url: "https://podme.herokuapp.com/",	
+		name: "Podme",
+		description: "Search iTunes for podcasts and organized them into lists.",
+		repoUrl:"https://github.com/thelmaboamah/Podme"
+		},
+		{
+		image:"assets/imgs/amplify-widget.png",
+		alt: "Screenshot of Amplify widget",
+		url: `${baseUrl}/amplify-widget/`,	
+		name: "Amplify Web Widget",
+		description: "A an embeddable web widget versions of the <a href='https://play.google.com/store/apps/details?id=org.getamplify.amplify' target='_blank' rel='noreferrer'>Amplify mobile app</a>. Winner of <a href='http://angelhack.com/' target='_blank' rel='noreferrer'>Angelhack Hackathon</a> in April 2017.",
+		repoUrl:"https://github.com/thelmaboamah/amplify-widget"
 		},
 		{
 			image:"assets/imgs/drumkit.png",
 			alt: "Screenshot of Questlove drumkit site",
-			url: "http://thelma.codes/drumkit/",	
+			url: `${baseUrl}/drumkit/`,	
 			name: "JS Drumkit",
 			description: "An interactive drumkit built with Vanilla JS as part of <a href='http://javascript30.com'>JavaScipt30</a>.",
 			repoUrl:"https://github.com/thelmaboamah/drumkit"
@@ -40,7 +58,7 @@ $(document).ready(function() {
 		{
 			image:"assets/imgs/airbnb.png",
 			alt: "Screenshot of Airbnb clone site",
-			url: "http://thelma.codes/airbnb_homepage_clone/",	
+			url: `${baseUrl}/airbnb_homepage_clone/`,	
 			name: "Airbnb Clone",
 			description:"A responsive clone of Airbnb's homepage that I built with HTML, CSS, and jQuery.",
 			repoUrl:"https://github.com/thelmaboamah/airbnb_homepage_clone"
@@ -49,7 +67,7 @@ $(document).ready(function() {
 
 	var thumbnails = "";
 	for (var i = 0; i < 3; i++ ) {
-		thumbnails += `<a class="thumbnail" href="work.html" target="_blank"><img src="${projects[i].image}" alt="${projects[i].alt}"/></a>`
+		thumbnails += `<a class="thumbnail" href="work.html" target="_blank" rel="noreferrer"><img src="${projects[i].image}" alt="${projects[i].alt}"/></a>`
 	}
 
 	$(".pf-thumbnails").append(thumbnails);
@@ -57,11 +75,11 @@ $(document).ready(function() {
 	var samples = "";
 	for (var i = 0; i < projects.length; i++) {
 		samples += `<div class="sample col-xs-12 col-md-6 col-lg-4">
-									<a href="${projects[i].url}" target="_blank"><img class="img-fluid" src="${projects[i].image}" alt="${projects[i].alt}"/></a>
+									<a href="${projects[i].url}" target="_blank" rel="noreferrer"><img class="img-fluid" src="${projects[i].image}" alt="${projects[i].alt}"/></a>
 									<div>
 										<h3>${projects[i].name}</h3>
 										<p>${projects[i].description}</p>
-										<a href="${projects[i].repoUrl}" target="_blank">Repo</a>
+										<a href="${projects[i].repoUrl}" target="_blank" rel="noreferrer">Repo</a>
 									</div>
 								</div>`
 	}
